fe8d379c21dfee0a95edef71ccf194cdf3e8a74a
John ZuHone <jzuhone@gmail.com>
2017-08-01 18:06:52 -0400

    Forgot to fix the MSID name

--- psmc_check/tests/test_psmc.py
+++ psmc_check/tests/test_psmc.py
@@ -8,5 +8,5 @@ default_model_spec = os.path.join(model_path, "psmc_model_spec.json")
 def test_psmc_may3016(answer_store):
     run_start = "2016:122:12:00:00.000"
     load_week = "MAY3016"
-    load_test_template("1deamzt", "psmc", answer_store, run_start,
+    load_test_template("1pdeaat", "psmc", answer_store, run_start,
                        load_week, default_model_spec, psmc_check)
