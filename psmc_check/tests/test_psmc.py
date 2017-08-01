3136b8dbd35c4abef25fafdecd78d64d030f2d97
John ZuHone <jzuhone@gmail.com>
2017-08-01 18:04:05 -0400

    Edit this test in line with test updates in acis_thermal_check

--- psmc_check/tests/test_psmc.py
+++ psmc_check/tests/test_psmc.py
@@ -1,27 +1,12 @@
-import os
-import shutil
-import tempfile
-
 from ..psmc_check import psmc_check, model_path
-from acis_thermal_check.regression_testing import run_answer_test, \
-    run_image_test, run_model
+from acis_thermal_check.regression_testing import \
+    load_test_template
+import os
 
-def psmc_test_template(generate_answers, run_start, load_week, 
-                       cmd_states_db='sybase'):
-    if generate_answers is not None:
-        generate_answers = os.path.abspath(generate_answers)
-    tmpdir = tempfile.mkdtemp()
-    curdir = os.getcwd()
-    os.chdir(tmpdir)
-    model_spec = os.path.join(model_path, "psmc_model_spec.json")
-    out_dir = run_model("psmc", psmc_check, model_spec, run_start, 
-                        load_week, cmd_states_db)
-    run_answer_test("psmc", load_week, out_dir, generate_answers)
-    run_image_test("1pdeaat", "psmc", load_week, out_dir, generate_answers)
-    os.chdir(curdir)
-    shutil.rmtree(tmpdir)
+default_model_spec = os.path.join(model_path, "psmc_model_spec.json")
 
-def test_psmc_may3016(generate_answers):
+def test_psmc_may3016(answer_store):
     run_start = "2016:122:12:00:00.000"
     load_week = "MAY3016"
-    psmc_test_template(generate_answers, run_start, load_week)
+    load_test_template("1deamzt", "psmc", answer_store, run_start,
+                       load_week, default_model_spec, psmc_check)
