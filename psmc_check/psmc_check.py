2659260288e505e8244ff279292b0ab45441efc0
John ZuHone <jzuhone@gmail.com>
2018-01-12 16:03:44 -0500

    We've now fixed it so we don't have to subclass

--- psmc_check/psmc_check.py
+++ psmc_check/psmc_check.py
@@ -38,8 +38,7 @@ VALIDATION_LIMITS = {'1PDEAAT': [(1, 2.5), (50, 1.0), (99, 5.5)],
 HIST_LIMIT = [30., 40.]
 
 def calc_model(model_spec, states, start, stop, T_psmc=None, T_psmc_times=None,
-               T_pin1at=None,T_pin1at_times=None,
-               dh_heater=None,dh_heater_times=None):
+               dh_heater=None, dh_heater_times=None):
     model = xija.XijaModel('psmc', start=start, stop=stop, model_spec=model_spec)
     times = np.array([states['tstart'], states['tstop']])
     model.comp['sim_z'].set_data(states['simpos'], times)
@@ -48,44 +47,27 @@ def calc_model(model_spec, states, start, stop, T_psmc=None, T_psmc_times=None,
     # 1PIN1AT is broken, so we set its initial condition
     # using an offset, which makes sense based on historical
     # data
-    if T_pin1at is None:
+    if T_psmc is None:
         T_pin1at = model.comp["1pdeaat"].dvals - 10.0
-    model.comp['pin1at'].set_data(T_pin1at,T_pin1at_times)
+    else:
+        T_pin1at = T_psmc - 10.0
+    model.comp['pin1at'].set_data(T_pin1at,T_psmc_times)
     model.comp['roll'].set_data(calc_off_nom_rolls(states), times)
     model.comp['eclipse'].set_data(False)
     for name in ('ccd_count', 'fep_count', 'vid_board', 'clocking', 'pitch'):
         model.comp[name].set_data(states[name], times)
-    model.comp['dh_heater'].set_data(dh_heater,dh_heater_times)
+    model.comp['dh_heater'].set_data(dh_heater, dh_heater_times)
     model.make()
     model.calc()
     return model
 
-class PSMCCheck(ACISThermalCheck):
-
-    def calc_model_wrapper(self, model_spec, states, tstart, tstop, state0=None):
-        if state0 is None:
-            start_msid = None
-            start_pin = None
-            dh_heater = None
-            dh_heater_times = None
-        else:
-            start_msid = state0[self.msid]
-            start_pin = state0[self.msid]-10.0 # the infamous pin1at hack
-            htrbfn = os.path.join(self.bsdir, 'dahtbon_history.rdb')
-            mylog.info('Reading file of dahtrb commands from file %s' % htrbfn)
-            htrb = ascii.read(htrbfn, format='rdb')
-            dh_heater_times = date2secs(htrb['time'])
-            dh_heater = htrb['dahtbon'].astype(bool)
-        return self.calc_model(model_spec, states, tstart, tstop, T_psmc=start_msid,
-                               T_psmc_times=None, T_pin1at=start_pin, T_pin1at_times=None,
-                               dh_heater=dh_heater, dh_heater_times=dh_heater_times)
 
 def main():
     args = get_options("psmc", model_path)
-    psmc_check = PSMCCheck("1pdeaat", "psmc", MSID, 
-                           VALIDATION_LIMITS, HIST_LIMIT, 
-                           calc_model, args, other_telem=['1dahtbon'],
-                           other_map={'1dahtbon': 'dh_heater'})
+    psmc_check = ACISThermalCheck("1pdeaat", "psmc", MSID, 
+                                  VALIDATION_LIMITS, HIST_LIMIT, 
+                                  calc_model, args, other_telem=['1dahtbon'],
+                                  other_map={'1dahtbon': 'dh_heater'})
     try:
         psmc_check.driver()
     except Exception as msg:
