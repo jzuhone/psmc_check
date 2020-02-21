3e005ea17628bbd1b75e6f68e027d1b136462344
John ZuHone <jzuhone@gmail.com>
2020-02-21 16:10:04 -0500

    Fix tests for atc3.0

--- psmc_check/tests/test_psmc.py
+++ psmc_check/tests/test_psmc.py
@@ -1,13 +1,20 @@
-from ..psmc_check import VALIDATION_LIMITS, \
-    HIST_LIMIT, calc_model, model_path
+from ..psmc_check import model_path, PSMCCheck
 from acis_thermal_check.regression_testing import \
-    RegressionTester
+    RegressionTester, all_loads
+import pytest
 
-atc_kwargs = {"other_telem": ['1dahtbon'],
-              "other_map": {'1dahtbon': 'dh_heater'}}
+psmc_rt = RegressionTester(PSMCCheck, model_path)
 
-psmc_rt = RegressionTester("1pdeaat", "psmc", model_path, VALIDATION_LIMITS,
-                           HIST_LIMIT, calc_model, atc_kwargs=atc_kwargs)
+# Prediction tests
 
-def test_psmc_loads(answer_store):
-    psmc_rt.run_test_arrays(answer_store)
+
+@pytest.mark.parametrize('load', all_loads)
+def test_prediction(answer_store, load):
+    psmc_rt.run_test("prediction", answer_store, load)
+
+# Validation tests
+
+
+@pytest.mark.parametrize('load', all_loads)
+def test_validation(answer_store, load):
+    psmc_rt.run_test("validation", answer_store, load)
